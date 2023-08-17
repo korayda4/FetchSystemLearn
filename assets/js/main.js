ee4a40d9fbc3a6b2aad3c9ad112c8de1d176651a
koray demir <koraydemirmc@gmail.com>
2023-08-17 22:33:43 +0300

    daha iyi kodlama ve bug düzeltimi

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,67 +8,67 @@ activeCommentsDiv.addEventListener("click", deleteCommentsUser);
 activeUserDiv.addEventListener("click", deleteShowUser);
 
 container.addEventListener("click", (e) => {
-    if (e.target.className === "userBtn") {
+    if (e.target.classList.contains("userBtn")) {
         showUser(e.target.id);
     } else {
         showComments(e);
     }
 });
 
-let AllUser;
-let AllComments;
+let allUsers;
+let allComments;
 
-fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(comments => {
-        AllComments = comments;
-        return fetch('https://jsonplaceholder.typicode.com/users');
-    })
-    .then(response => response.json())
-    .then(users => {
-        AllUser = users;
-        return fetch('https://jsonplaceholder.typicode.com/posts');
-    })
-    .then(response => response.json())
-    .then(posts => {
+async function fetchData(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+
+async function initializeData() {
+    try {
+        allComments = await fetchData('https://jsonplaceholder.typicode.com/comments');
+        allUsers = await fetchData('https://jsonplaceholder.typicode.com/users');
+        const posts = await fetchData('https://jsonplaceholder.typicode.com/posts');
         addPost(posts);
-    });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
 function addPost(posts) {
-    let valueName;
+    let previousUserName = "";
     
-    let value = 0;
+    posts.forEach((post) => {
+        const user = allUsers.find((u) => u.id === post.userId);
 
-    posts.forEach((element) => {
-        const userName = AllUser.find((x) => x.id === element.userId);
-
-        container.innerHTML += `
-            <div class="post">
-                <div class="name">
-                    <h2>${userName.name}</h2>
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+        postElement.innerHTML = `
+            <div class="name">
+                <h2>${user.name}</h2>
+            </div>
+            <div class="postComment">
+                <h4>${post.body}</h4>
+            </div>
+            <div class="stats">
+                <div class="userStats">
+                    <img src="assets/image/icons8-user-32.png" alt="" class="userBtn" id="${user.name}">
                 </div>
-                <div class="postComment">
-                    <h4>${element.body}</h4>
-                </div>
-                <div class="stats">
-                    <div class="userStats">
-                        <img src="assets/image/icons8-user-32.png" alt="" class="userBtn" id="${userName.name}">
-                    </div>
-                    <div class="comments">
-                        <img src="assets/image/icons8-comments-32.png" class="commentsBtn" id="${element.id}" alt="">
-                    </div>
+                <div class="comments">
+                    <img src="assets/image/icons8-comments-32.png" class="commentsBtn" id="${post.id}" alt="">
                 </div>
             </div>`;
 
-        if (valueName === userName.name) {
+        if (previousUserName === user.name) {
             value++;
         }
-        valueName = userName.name;
+        previousUserName = user.name;
+
+        container.appendChild(postElement);
     });
 }
 
-function showUser(event) {
-    let activeUser = AllUser.find((x) => x.name === event);
+function showUser(eventName) {
+    const activeUser = allUsers.find((user) => user.name === eventName);
 
     activeUserDiv.innerHTML = `
         <div class="showUser">
@@ -94,35 +94,33 @@ function showUser(event) {
 }
 
 function deleteShowUser(e) {
-    if (e.target.className === "cancelBtn") {
+    if (e.target.classList.contains("cancelBtn")) {
         activeUserDiv.innerHTML = "";
     }
 }
 
 function deleteCommentsUser(e) {
-    if (e.target.className === "cancelBtn") {
+    if (e.target.classList.contains("cancelBtn")) {
         activeCommentsDiv.innerHTML = "";
     }
 }
 
 function showComments(e) {
-    console.log(e.target.id);
-    let activeComments = AllComments.filter((x) => x.postId == e.target.id);
+    const postId = e.target.id;
+    const activeComments = allComments.filter((comment) => comment.postId == postId);
 
-    let commentsHTML = "";
-
-    activeComments.forEach((x) => {
-        commentsHTML += `
-            <div class="comment">
-                <div class="cancelBtn">✗</div>
-                <div class="name">
-                    <h3>${x.name}</h3>
-                </div>
-                <div class="commentBody">
-                    <h5>${x.body}</h5>
-                </div>
-            </div>`;
-    });
+    const commentsHTML = activeComments.map((comment) => `
+        <div class="comment">
+            <div class="cancelBtn">✗</div>
+            <div class="name">
+                <h3>${comment.name}</h3>
+            </div>
+            <div class="commentBody">
+                <h5>${comment.body}</h5>
+            </div>
+        </div>`).join('');
 
     activeCommentsDiv.innerHTML = commentsHTML;
 }
+
+initializeData();
